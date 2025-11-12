fe23d6eacd00aaa45aa74b91874076f1be8344e4
DESKTOP-6NHL7HB\arifh <arifhamim11@gmail.com>
2025-11-12 23:23:08 +0600

    login user store in db

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -33,6 +33,17 @@ const Login = () => {
     const handleGoogleSubmit = () => {
         signInGoogle()
         .then(result => {
+            const newUser = {
+                name: result.user.displayName,
+                email: result.user.email,
+                photo: result.user.photoURL
+            }
+            fetch('http://localhost:3000/users',{
+                method: "POST",
+                headers:  {'content-type' : 'application/json'},
+                body: JSON.stringify(newUser)
+            })
+
             navigate(from,{replace: true})
             console.log(result.user)
         })
